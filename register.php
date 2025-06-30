9c3c821a922d28400ea100fc51cfcbbdf17b8d85
Altoid <102745236+Raynnerkavulika@users.noreply.github.com>
2025-06-30 17:00:57 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,46 @@
 <?php
-include "config.php";
+include('config.php');
+
+if(isset($_POST['submit'])){
+    
+    $name = $_POST['name'];
+    $name = filter_var($name,FILTER_SANITIZE_STRING);
+    $email = $_POST['email'];
+    $email = filter_var($email,FILTER_SANITIZE_STRING);
+    $password = $_POST['password'];
+    $password = filter_var($password,FILTER_SANITIZE_STRING);
+    $cpassword = $_POST['cpassword'];
+    $cpassword = filter_var($cpassword,FILTER_SANITIZE_STRING);
+
+    $image = $_FILES['image']['name'];
+    $image_size = $_FILES['image']['size'];
+    $image_tmp_name = $_FILES['image']['tmp_name'];
+    $image_folder = 'uploaded_img/'.$image;
+
+    $select = $conn->prepare("SELECT * FROM users WHERE email=?" );
+    $select->execute([$email]);
+
+    if($select->rowCount()>0){
+        $message[] = 'user already exist';
+    }else{
+        if($password != $cpassword){
+            $message[] = 'confirm password does not match';
+        }else{
+            $insert = $conn->prepare("INSERT INTO users(name,email,password,image) VALUES(?,?,?,?)");
+            $insert->execute([$name,$email,$password,$image]);
+
+            if($insert){
+                if($image_size>2000000){
+                    $message[] = 'image size too large';
+                }else{
+                    move_uploaded_file($image_tmp_name,$image_folder);
+                    header('location:login.php');
+                    $message[] = 'registered successfully';
+                }
+            }
+        }
+    }
+}
 
 
 ?>
