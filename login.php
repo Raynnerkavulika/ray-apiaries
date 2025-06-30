533052fb40c39a8e0e89242507af6eb36bfb6982
Altoid <102745236+Raynnerkavulika@users.noreply.github.com>
2025-06-30 17:09:38 +0300

    Update login.php

--- login.php
+++ login.php
@@ -34,3 +34,29 @@ if(isset($_POST['submit'])){
 }
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="form-container">
+        <form action="" method="post" enctype="multipart/form-data">
+            <h3><i class="fas fa-smile"></i> welcome back!</h3>
+            <input type="email" placeholder="enter your email" name="email" required class="box">
+            <input type="password" placeholder="enter your password" name="password" required class="box">
+            <input type="submit" value="login now" name="submit" required class="btn">
+            <p>don't have an account? <a href="register.php">register now</a></p>
+        </form>
+    </section>
+</body>
+</html>
